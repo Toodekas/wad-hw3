2055fd4a5caa679edec8026fa0baf60afc55fb0e
Martin Toode <toodemartin@gmail.com>
2019-11-10 19:26:14 +0200

    Added vue components

---
+++ src/components/Courses_add_course.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Courses_add_course"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Profile_view.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Profile_view"
+    }
+</script>
+
+<style scoped>
+
+</style>
